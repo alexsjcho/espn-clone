c8c005a5b00788dde262dcc739942679ccd0cb54
Alex Cho <alexsjcho@gmail.com>
2018-10-20 13:24:00 -0700

    add, edit, delete

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,7 +1,7 @@
 class CommentController < ApplicationController
-
+before_action :find_message, only: [:create, :edit, :update, :destroy]
+before_action :find_comment, only: [:edit, :update, :destroy]
   def create
-    @message = Message.find(params[:message_id])
     @comments = @message.comments.create(comment_params)
     @comment.user_id = current_user.user_id
 
@@ -12,11 +12,35 @@ class CommentController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to message_path(@nessage)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to message_path(@message)
+  end
 
 private
 
 def comment_params
   params.require(:comment).permit(:content)
 end
+
+def find_message
+  @message = Message.find(params[:message_id])
+end
+
+def find_comment
+  @comment = @message.comments.find(params[:id])
+end
+
 
 end
